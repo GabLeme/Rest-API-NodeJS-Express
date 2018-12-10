60488a4735d9c82572a297b40182c585c55d0e09
Gabriel Leme <37668239+GabLeme@users.noreply.github.com>
2018-12-10 09:15:25 -0200

    Update ApiREST.js

--- ApiREST.js
+++ ApiREST.js
@@ -2,7 +2,7 @@ require('dotenv-safe').load();
 const express = require('express');
 const app = express();         
 const bodyParser = require('body-parser');
-const port = process.env.POR;
+const port = process.env.PORT;
 const sql = require('mssql');
 const connStr = process.env.sqlConnection;
 
